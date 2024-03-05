9dbe8a404acbb58c58b7bbdf23f25f7b46f66f75
Aisha-Halane <caaishocali10@gmail.com>
2024-03-05 11:00:33 +0300

    update code

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,35 +1,40 @@
 import {useState} from 'react'
 
 function Header(){
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen , setIsOpen] = useState(false)
+    const handleIsOpen = () =>
+{
+    setIsOpen(true)
+}
+const handleClose = () => {
+    setIsOpen(false)
+}
+  return(
+        <div className="">
+        <div className="bg-teal-500 p-5 flex justify-between text-white">
+            <h1 className="font-semibold text-white text-center text-3xl">Light</h1>
+            <i style={{display : isOpen == true ? "none" : " "}} onClick={handleIsOpen} class="fa-solid fa-bars text-5xl font-bold sm:hidden"></i>
+            <i onClick={handleClose} style={{display : isOpen == true ? "block" : "none"}} class="fa-solid fa-x text-5xl font-bold"></i>
+             <ul className="hidden sm:flex gap-16 text-2xl ">
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Contact</li>
+             </ul>
+             
+             
 
-    const handleOpen =()=>{
-        setIsOpen(true)
-    }
-
-
-    return <div>
-    <div className="  bg-green-700 flex justify-between p-6 text-white ">
-      <h1 className="text-5xl font-bold">Tasbih </h1>
-      <i onClick={handleOpen} class="fa-solid fa-bars text-5xl sm:hidden"></i>
-
-      <ul className="hidden sm:flex gap-16 text-3xl">
-        <li>Home</li>
-        <li>About</li>
-        <li>Service</li>
-        <li>Blogs</li>
-      </ul>
-      </div>
-      
-      <ul style={{display: isOpen == true ? "flex" : "" }} className=" hidden  bg-orange-400 text-white space-y-5 flex-col p-2  text-3xl">
-        <li>Home</li>
-        <li>About</li>
-        <li>Service</li>
-        <li>Blogs</li>
-      </ul>
-      
-    </div>
+          </div>
+          <div className="">
+              <ul style={{display : isOpen == true ? "flex" : "" }} className="hidden absolute w-full bg-orange-500 text-white flex-col space-y-3  gap-16 text-2xl ">
+                <li>Home</li>
+                <li>About</li>
+                <li>Service</li>
+                <li>Contact</li>
+             </ul>
+          </div>
+        </div>
         
-    
+    )
 }
 export default Header;
